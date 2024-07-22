423e8ae307b99d148b1b80e90b04267060aa76a0
Ruan vHeerden <vanheerdenruan43@gmail.com>
2024-07-22 15:38:07 +0200

    added more fields to columns

--- models.py
+++ models.py
@@ -14,9 +14,15 @@ class Bowler(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     runs_given = db.Column(db.Integer, default=0)
     balls_bowled = db.Column(db.Integer, default=0)
+    four_runs = db.Column(db.Integer, default=0)
+    six_runs = db.Column(db.Integer, default=0)
+    no_ball = db.Column(db.Integer, default=0)
+    wide_ball = db.Column(db.Integer, default=0)
 
 
 class Balls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    bowler = db.Column(db.String(100), nullable=False)  # Make sure this line is present
+    bowler = db.Column(db.String(100), nullable=False)
     runs = db.Column(db.Integer, default=0)
+    no_ball = db.Column(db.Integer, default=0)
+    wide_ball = db.Column(db.Integer, default=0)
