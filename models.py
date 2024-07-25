6c8c5e46856df44cf9a058589430e2780a3c49be
Ruan vHeerden <vanheerdenruan43@gmail.com>
2024-07-25 15:57:49 +0200

    model update for table

--- models.py
+++ models.py
@@ -12,17 +12,14 @@ db = SQLAlchemy()
 class Bowler(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    runs_given = db.Column(db.Integer, default=0)
-    balls_bowled = db.Column(db.Integer, default=0)
-    four_runs = db.Column(db.Integer, default=0)
-    six_runs = db.Column(db.Integer, default=0)
-    no_ball = db.Column(db.Integer, default=0)
-    wide_ball = db.Column(db.Integer, default=0)
 
 
 class Balls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     bowler = db.Column(db.String(100), nullable=False)
+    batsman = db.Column(db.String(100), nullable=False)
     runs = db.Column(db.Integer, default=0)
-    no_ball = db.Column(db.Integer, default=0)
-    wide_ball = db.Column(db.Integer, default=0)
+    no_ball = db.Column(db.Boolean, default=False)
+    wide_ball = db.Column(db.Boolean, default=False)
+    four_runs = db.Column(db.Boolean, default=False)
+    six_runs = db.Column(db.Boolean, default=False)
