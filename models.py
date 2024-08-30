a358cd36558a3fd135d8b8bc091fbe12611fc82d
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-08-30 09:21:48 +0200

    Update models.py
    Co-authored-by: Jaster <29538392+JasterMareel@users.noreply.github.com>

--- models.py
+++ models.py
@@ -3,13 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# class Batsman(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     runs = db.Column(db.Integer, default=0)
-#     balls_faced = db.Column(db.Integer, default=0)
-
-
 class Batsman(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
